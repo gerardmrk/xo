4007a84a8c8d3b070b2a19ee379776a67cb61e56
Gerard Chin <gerardchin91@gmail.com>
2018-07-17 23:09:00 +1200

    Remove some prettier-ignore directives since printWidth rule has been modified

--- src/client/store/session/actions.ts
+++ src/client/store/session/actions.ts
@@ -9,37 +9,31 @@ import { createAction } from "typesafe-actions";
 import { MiddlewareFlags } from "@client/store/middleware";
 import { AuthTokens } from "@client/store/session/models";
 
-// prettier-ignore
 export const loginPending = createAction(
   "session.loginPending",
   resolve => (flags: MiddlewareFlags) => resolve(undefined, flags)
 );
 
-// prettier-ignore
 export const loginSuccess = createAction(
   "session.loginSuccess",
   resolve => (tokens: AuthTokens, flags: MiddlewareFlags) => resolve(tokens, flags)
 );
 
-// prettier-ignore
 export const loginFailure = createAction(
   "session.loginFailure",
   resolve => (error: Error, flags: MiddlewareFlags) => resolve(error, flags)
 );
 
-// prettier-ignore
 export const logoutPending = createAction(
   "session.logoutPending",
   resolve => (flags: MiddlewareFlags) => resolve(undefined, flags)
 );
 
-// prettier-ignore
 export const logoutSuccess = createAction(
   "session.logoutSuccess",
   resolve => (flags: MiddlewareFlags) => resolve(undefined, flags)
 );
 
-// prettier-ignore
 export const logoutFailure = createAction(
   "session.logoutFailure",
   resolve => (error: Error, flags: MiddlewareFlags) => resolve(error, flags)
